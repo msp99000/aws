391424ce1e506372efed86d3e18c6b784a1bb852
Manpreet Singh <msp99000@gmail.com>
2025-04-16 23:18:52 +0530

    feat: ML Model

---
+++ ec2/classifier.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load the iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, "iris_model.pkl")
